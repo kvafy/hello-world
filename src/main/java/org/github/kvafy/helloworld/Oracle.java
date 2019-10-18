1c46a8c3a3b2f15269f6af3b81a09330177887f0
David Chaloupka <chaloupka.david@gmail.com>
2019-10-18 10:08:55 +0100

    [maven-release-plugin] prepare release hello-world-0.1

--- src/main/java/org/github/kvafy/helloworld/Oracle.java
+++ src/main/java/org/github/kvafy/helloworld/Oracle.java
@@ -3,7 +3,12 @@ package org.github.kvafy.helloworld;
 /** The all-knowing Oracle. */
 public final class Oracle {
 
-  /** Answers the most important questions in the whole world. */
+  /**
+   * Answers the most important questions in the whole world.
+   *
+   * @param question any question
+   * @return an answer to the given question
+   */
   public String getAnswer(String question) {
     return "42";
   }
